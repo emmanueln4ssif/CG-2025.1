3a127f1a1a9510d797a7d87dd07f5383f06b1c84
Emmanuel Nassif <emmanuel.nassif@gmail.com>
2025-05-21 20:27:44 -0300

    feat: walls and positions

--- T1/T1.js
+++ T1/T1.js
@@ -2,6 +2,7 @@ import * as THREE from 'three';
 import { OrbitControls } from '../build/jsm/controls/OrbitControls.js';
 import { PointerLockControls } from '../build/jsm/controls/PointerLockControls.js';
 import { Clock } from 'three';
+import KeyboardState from '../libs/util/KeyboardState.js';
 import {
   initRenderer,
   initCamera,
@@ -20,50 +21,59 @@ material = setDefaultMaterial(); // create a basic material
 light = initDefaultBasicLight(scene); // Create a basic light to illuminate the scene
 orbit = new OrbitControls(camera, renderer.domElement); // Enable mouse rotation, pan, zoom etc.
 const clock = new Clock(); // Create a clock to manage time
+var keyboard = new KeyboardState();
 
 // Listen window size changes
 window.addEventListener('resize', function () { onWindowResize(camera, renderer) }, false);
 
 // ** Creating the plane ** 
 let plane = createGroundPlaneXZ(500, 500)
+plane.material.color.set(0xFFF1C1); // siena (marrom-terra)
 scene.add(plane);
 
+//Adicionando plataformas
+const area1 = buildPlatform(scene, 100, 120, 8, { x: 160, y: 0, z: 150 }, 15, 8, 1, 0x7FFFD4);
+const area2 = buildPlatform(scene, 100, 120, 10, { x: 10, y: 0, z: 150 }, 15, 8, 1, 0xE1A4A0);
+const area3 = buildPlatform(scene, 100, 120, 8, { x: -150, y: 0, z: 150 }, 15, 8, 1, 0xC3D3F1);
+const area4 = buildPlatform(scene, 100, 320, 8, { x: 10, y: 0, z: -150 }, 15, 8, 1, 0xB9D7A9);
 
-const area1 = buildPlatform(100, 120, 8, { x: 160, y: 0, z: 100 }, 15, 8, 1, 0x7FFFD4);
-const area2 = buildPlatform(100, 120, 10, { x: 0, y: 0, z: 100 }, 15, 8, 1, 0x0FF7F7F);
-const area3 = buildPlatform(100, 120, 8, { x: -160, y: 0, z: 100 }, 15, 8, 1, 0xadd8e6);
-const area4 = buildPlatform(120, 30, 8, { x: 120, y: 0, z: -100 }, 15, 8, 1);
+//Rotação da area 4 para escada se adequar a plataforma (rever isso!)
+const box = new THREE.Box3().setFromObject(area4);
+const center = box.getCenter(new THREE.Vector3());
+area4.rotateY(-Math.PI);
+area4.position.set(10, center.y, -300);
 
-scene.add(area1);
-scene.add(area2);
-scene.add(area3);
+//Adicionando as paredes (depois precisa aperfeiçoar isso)
+const wall_height = 30;
+const wall_thickness = 5;
+const wallColor = 0x8B4513;
+addWallsAroundPlane(scene, 500, wall_height, wall_thickness, wallColor);
 
 render();
 
 function render() {
+  keyboardUpdate();
   requestAnimationFrame(render);
   renderer.render(scene, camera) // Render scene
 }
 
 ///////////////////////////////////////////////////////////////////////////////
-//degrau n acompanha altura da plataforma: ok
 //interseção entre primeiro degrau e plataforma
 //mudança de lado da escada
 //mudança de orientação da plataforma
 
-
-function buildPlatform(side_size, front_size, height, position, step_size, number_of_steps, step_depth, color) {
+function buildPlatform(scene, side_size, front_size, height, position, step_size, number_of_steps, step_depth, color) {
   const stair_depth = number_of_steps * step_depth;
   const step_height = height / number_of_steps;
   const depth = height;
-  const platform  = new THREE.Group();
+  const platform = new THREE.Group();
   const escadaGroup = new THREE.Group();
 
   for (let i = 0; i < number_of_steps; i++) {
 
     const degrau = new THREE.Mesh(
       new THREE.BoxGeometry(step_size, step_height, step_depth),
-      new THREE.MeshStandardMaterial({ color: 0xFFFFFF })
+      setDefaultMaterial('white')
     );
 
     degrau.position.set(0, (i + 0.5) * step_height, (i + 0.5) * step_depth);
@@ -71,54 +81,84 @@ function buildPlatform(side_size, front_size, height, position, step_size, numbe
   }
 
   escadaGroup.position.set(position.x, 0, position.z - (side_size / 2) + (step_depth / 2));
-  console.log(escadaGroup.position);
+  //console.log(escadaGroup.position);
 
   const boundingBox = new THREE.Box3().setFromObject(escadaGroup);
   const size = boundingBox.getSize(new THREE.Vector3());
   const center = boundingBox.getCenter(new THREE.Vector3());
 
-  //scene.add(escadaGroup);
-
-  // Plataforma frontal 1
   const frontal1 = new THREE.Mesh(
     new THREE.BoxGeometry((front_size - step_size) / 2, height, depth),
-    new THREE.MeshStandardMaterial({ color: color })
+    setDefaultMaterial(color)
   );
 
   const boundingBox1 = new THREE.Box3().setFromObject(frontal1);
   const size1 = boundingBox1.getSize(new THREE.Vector3());
 
   frontal1.position.set(center.x - size1.x / 2 - step_size / 2, height / 2, center.z);
-  console.log(frontal1.position);
-  //scene.add(frontal1);
 
-  // Plataforma frontal 2
   const frontal2 = new THREE.Mesh(
     new THREE.BoxGeometry((front_size - step_size) / 2, height, depth),
-    new THREE.MeshStandardMaterial({ color: color })
+    setDefaultMaterial(color)
   );
 
   const boundingBox2 = new THREE.Box3().setFromObject(frontal2);
   const size2 = boundingBox2.getSize(new THREE.Vector3());
-
   frontal2.position.set(center.x + size2.x / 2 + step_size / 2, height / 2, center.z);
-  //scene.add(frontal2);
 
   const traseira = new THREE.Mesh(
     new THREE.BoxGeometry(front_size, height, side_size - stair_depth),
-    new THREE.MeshStandardMaterial({ color: color })
+    setDefaultMaterial(color)
   );
 
   traseira.position.set(center.x, height / 2, center.z + (side_size / 2) - (step_depth / 2));
-  //scene.add(traseira);
 
-  platform.add(escadaGroup);
-  platform.add(frontal1);
-  platform.add(frontal2);
-  platform.add(traseira);
+  platform.add(escadaGroup, frontal1, frontal2, traseira);
+  scene.add(platform);
 
   return platform;
 
+}
+
+function createWall(width, height, depth, color) {
+
+  const geometry = new THREE.BoxGeometry(width, height, depth);
+  const material = setDefaultMaterial(color);
+  const wall = new THREE.Mesh(geometry, material);
+
+  return wall;
+}
+
+function addWallsAroundPlane(scene, plane_size, wall_height, wall_thickness, color) {
+
+  const half = plane_size / 2;
+
+  const wall_front = createWall(plane_size, wall_height, wall_thickness, color);
+  wall_front.position.set(0, wall_height / 2, half + wall_thickness / 2);
+
+  const wall_back = createWall(plane_size, wall_height, wall_thickness, color);
+  wall_back.position.set(0, wall_height / 2, -half - wall_thickness / 2);
+
+  const wall_left = createWall(wall_thickness, wall_height, plane_size, color);
+  wall_left.position.set(-half - wall_thickness / 2, wall_height / 2, 0);
+
+  const wall_right = createWall(wall_thickness, wall_height, plane_size, color);
+  wall_right.position.set(half + wall_thickness / 2, wall_height / 2, 0);
+
+  scene.add(wall_front, wall_back, wall_left, wall_right);
+
+}
+
+//provisório 
+function keyboardUpdate() {
+   keyboard.update();
+   if (keyboard.pressed("left")) camera.position.x += 1;
+   if (keyboard.pressed("right")) camera.position.x -= 1;
+   if (keyboard.pressed("up")) camera.position.z += 1;
+   if (keyboard.pressed("down")) camera.position.z -= 1;
+   if (keyboard.pressed("pagedown")) camera.position.y -= 1;
+   if (keyboard.pressed("pageup")) camera.position.y += 1;
+  
 }
 
 
--- examples/geometricTransformation1.js
+++ examples/geometricTransformation1.js
@@ -1,14 +1,16 @@
-import * as THREE from  'three';
+import * as THREE from 'three';
 import { OrbitControls } from '../build/jsm/controls/OrbitControls.js';
 import KeyboardState from '../libs/util/KeyboardState.js';
-import {initRenderer, 
-        initCamera,
-        initDefaultBasicLight,
-        setDefaultMaterial,
-        InfoBox,
-        SecondaryBox,        
-        onWindowResize, 
-        createGroundPlaneXZ} from "../libs/util/util.js";
+import {
+   initRenderer,
+   initCamera,
+   initDefaultBasicLight,
+   setDefaultMaterial,
+   InfoBox,
+   SecondaryBox,
+   onWindowResize,
+   createGroundPlaneXZ
+} from "../libs/util/util.js";
 
 let scene, renderer, camera, material, light, orbit;; // Initial variables
 scene = new THREE.Scene();    // Create main scene
@@ -16,10 +18,10 @@ renderer = initRenderer();    // Init a basic renderer
 camera = initCamera(new THREE.Vector3(0, 15, 30)); // Init camera in this position
 material = setDefaultMaterial(); // create a basic material
 light = initDefaultBasicLight(scene); // Create a basic light to illuminate the scene
-orbit = new OrbitControls( camera, renderer.domElement ); // Enable mouse rotation, pan, zoom etc.
+orbit = new OrbitControls(camera, renderer.domElement); // Enable mouse rotation, pan, zoom etc.
 
 // Listen window size changes
-window.addEventListener( 'resize', function(){onWindowResize(camera, renderer)}, false );
+window.addEventListener('resize', function () { onWindowResize(camera, renderer) }, false);
 
 // Use to scale the cube
 var scale = 1.0;
@@ -38,11 +40,11 @@ showInformation();
 var keyboard = new KeyboardState();
 
 // Show axes (parameter is size of each axis)
-var axesHelper = new THREE.AxesHelper( 1200 );
-scene.add( axesHelper );
+var axesHelper = new THREE.AxesHelper(1200);
+scene.add(axesHelper);
 
-// create the ground plane
-let plane = createGroundPlaneXZ(500, 500)
+// create a plane
+let plane = createGroundPlaneXZ(500, 500);
 scene.add(plane);
 
 // create a cube
@@ -59,12 +61,10 @@ cube.add(cubeAxesHelper);
 var positionMessage = new SecondaryBox("");
 positionMessage.changeStyle("rgba(0,0,0,0)", "lightgray", "16px", "ubuntu")
 
-const area1 = buildPlatform(100, 120, 8, { x: 160, y: 0, z: 150 }, 15, 8, 1, 0x7FFFD4);
-const area2 = buildPlatform(100, 120, 10, { x: 10, y: 0, z: 150 }, 15, 8, 1, 0x0FF7F7F);
-const area3 = buildPlatform(100, 120, 8, { x: -150, y: 0, z: 150 }, 15, 8, 1, 0xadd8e6);
-const area4 = buildPlatform(100, 320, 8, { x: 10, y: 0, z: -150 }, 15, 8, 1, 0x006400);
-
-scene.add(area1, area2, area3, area4);
+const area1 = buildPlatform(scene, 100, 120, 8, { x: 160, y: 0, z: 150 }, 15, 8, 1, 0x7FFFD4);
+const area2 = buildPlatform(scene, 100, 120, 10, { x: 10, y: 0, z: 150 }, 15, 8, 1, 0x0FF7F7F);
+const area3 = buildPlatform(scene, 100, 120, 8, { x: -150, y: 0, z: 150 }, 15, 8, 1, 0xadd8e6);
+const area4 = buildPlatform(scene, 100, 320, 8, { x: 10, y: 0, z: -150 }, 15, 8, 1, 0x006400);
 
 //Rotação da area 4 para escada se adequar a plataforma (rever isso!)
 const box = new THREE.Box3().setFromObject(area4);
@@ -72,63 +72,63 @@ const center = box.getCenter(new THREE.Vector3());
 area4.rotateY(-Math.PI);
 area4.position.set(10, center.y, -300);
 
+//Adicionando as paredes (depois precisa aperfeiçoar isso)
+const wall_height = 30;
+const wall_thickness = 5;
+const wallColor = 0x8B4513;
+addWallsAroundPlane(scene, 500, wall_height, wall_thickness, wallColor);
+
 render();
 
-function keyboardUpdate() 
-{
+function keyboardUpdate() {
    keyboard.update();
-   if ( keyboard.pressed("left") )     camera.position.x += 1;
-   if ( keyboard.pressed("right") )    camera.position.x -= 1;
-   if ( keyboard.pressed("up") )       camera.position.z += 1;
-   if ( keyboard.pressed("down") )     camera.position.z -= 1;
-   if ( keyboard.pressed("pagedown") )     camera.position.y -= 1;
-   if ( keyboard.pressed("pageup") )   camera.position.y += 1;
-   if ( keyboard.pressed("pagedown") ) cube.translateZ( -1 );
+   if (keyboard.pressed("left")) camera.position.x += 1;
+   if (keyboard.pressed("right")) camera.position.x -= 1;
+   if (keyboard.pressed("up")) camera.position.z += 1;
+   if (keyboard.pressed("down")) camera.position.z -= 1;
+   if (keyboard.pressed("pagedown")) camera.position.y -= 1;
+   if (keyboard.pressed("pageup")) camera.position.y += 1;
+   if (keyboard.pressed("pagedown")) cube.translateZ(-1);
 
-   let angle = THREE.MathUtils.degToRad(10); 
-   if ( keyboard.pressed("A") )  cube.rotateY(  angle );
-   if ( keyboard.pressed("D") )  cube.rotateY( -angle );
+   let angle = THREE.MathUtils.degToRad(10);
+   if (keyboard.pressed("A")) cube.rotateY(angle);
+   if (keyboard.pressed("D")) cube.rotateY(-angle);
 
-   if ( keyboard.pressed("W") )
-   {
-      scale+=.1;
+   if (keyboard.pressed("W")) {
+      scale += .1;
       cube.scale.set(scale, scale, scale);
    }
-   if ( keyboard.pressed("S") )
-   {
-      scale-=.1;
+   if (keyboard.pressed("S")) {
+      scale -= .1;
       cube.scale.set(scale, scale, scale);
-   }   
+   }
    updatePositionMessage();
 }
 
-function updatePositionMessage()
-{
-   var str =  "POS {" + cube.position.x.toFixed(1) + ", " + cube.position.y.toFixed(1) + ", " + cube.position.z.toFixed(1) + "} " + 
-             "| SCL {" + cube.scale.x.toFixed(1) + ", " + cube.scale.y.toFixed(1) + ", " + cube.scale.z.toFixed(1) + "} " + 
-             "| ROT {" + cube.rotation.x.toFixed(1) +  ", " + cube.rotation.y.toFixed(1) + ", " + cube.rotation.z.toFixed(1) + "}";              
+function updatePositionMessage() {
+   var str = "POS {" + cube.position.x.toFixed(1) + ", " + cube.position.y.toFixed(1) + ", " + cube.position.z.toFixed(1) + "} " +
+      "| SCL {" + cube.scale.x.toFixed(1) + ", " + cube.scale.y.toFixed(1) + ", " + cube.scale.z.toFixed(1) + "} " +
+      "| ROT {" + cube.rotation.x.toFixed(1) + ", " + cube.rotation.y.toFixed(1) + ", " + cube.rotation.z.toFixed(1) + "}";
    positionMessage.changeMessage(str);
 }
 
 
-function showInformation()
-{
-  // Use this to show information onscreen
-  var controls = new InfoBox();
-    controls.add("Geometric Transformation");
-    controls.addParagraph();
-    controls.add("Use keyboard arrows to move the cube in XY.");
-    controls.add("Press Page Up or Page down to move the cube over the Z axis");
-    controls.add("Press 'A' and 'D' to rotate.");
-    controls.add("Press 'W' and 'S' to change scale");
-    controls.show();
+function showInformation() {
+   // Use this to show information onscreen
+   var controls = new InfoBox();
+   controls.add("Geometric Transformation");
+   controls.addParagraph();
+   controls.add("Use keyboard arrows to move the cube in XY.");
+   controls.add("Press Page Up or Page down to move the cube over the Z axis");
+   controls.add("Press 'A' and 'D' to rotate.");
+   controls.add("Press 'W' and 'S' to change scale");
+   controls.show();
 }
 
-function render()
-{
-  keyboardUpdate();
-  requestAnimationFrame(render); // Show events
-  renderer.render(scene, camera) // Render scene
+function render() {
+   keyboardUpdate();
+   requestAnimationFrame(render); // Show events
+   renderer.render(scene, camera) // Render scene
 }
 
 ///////////////////////////////////////////////////////////////////////////////
@@ -138,71 +138,90 @@ function render()
 //mudança de orientação da plataforma
 
 
-function buildPlatform(side_size, front_size, height, position, step_size, number_of_steps, step_depth, color) {
-  const stair_depth = number_of_steps * step_depth;
-  const step_height = height / number_of_steps;
-  const depth = height;
-  const platform  = new THREE.Group();
-  const escadaGroup = new THREE.Group();
+function buildPlatform(scene, side_size, front_size, height, position, step_size, number_of_steps, step_depth, color) {
+   const stair_depth = number_of_steps * step_depth;
+   const step_height = height / number_of_steps;
+   const depth = height;
+   const platform = new THREE.Group();
+   const escadaGroup = new THREE.Group();
 
-  for (let i = 0; i < number_of_steps; i++) {
+   for (let i = 0; i < number_of_steps; i++) {
 
-    const degrau = new THREE.Mesh(
-      new THREE.BoxGeometry(step_size, step_height, step_depth),
-      new THREE.MeshStandardMaterial({ color: 0xFFFFFF })
-    );
+      const degrau = new THREE.Mesh(
+         new THREE.BoxGeometry(step_size, step_height, step_depth),
+         setDefaultMaterial('white')
+      );
 
-    degrau.position.set(0, (i + 0.5) * step_height, (i + 0.5) * step_depth);
-    escadaGroup.add(degrau);
-  }
+      degrau.position.set(0, (i + 0.5) * step_height, (i + 0.5) * step_depth);
+      escadaGroup.add(degrau);
+   }
 
-  escadaGroup.position.set(position.x, 0, position.z - (side_size / 2) + (step_depth / 2));
-  //console.log(escadaGroup.position);
+   escadaGroup.position.set(position.x, 0, position.z - (side_size / 2) + (step_depth / 2));
+   //console.log(escadaGroup.position);
 
-  const boundingBox = new THREE.Box3().setFromObject(escadaGroup);
-  const size = boundingBox.getSize(new THREE.Vector3());
-  const center = boundingBox.getCenter(new THREE.Vector3());
+   const boundingBox = new THREE.Box3().setFromObject(escadaGroup);
+   const size = boundingBox.getSize(new THREE.Vector3());
+   const center = boundingBox.getCenter(new THREE.Vector3());
 
-  //scene.add(escadaGroup);
+   const frontal1 = new THREE.Mesh(
+      new THREE.BoxGeometry((front_size - step_size) / 2, height, depth),
+      setDefaultMaterial(color)
+   );
 
-  // Plataforma frontal 1
-  const frontal1 = new THREE.Mesh(
-    new THREE.BoxGeometry((front_size - step_size) / 2, height, depth),
-    new THREE.MeshStandardMaterial({ color: color })
-  );
+   const boundingBox1 = new THREE.Box3().setFromObject(frontal1);
+   const size1 = boundingBox1.getSize(new THREE.Vector3());
 
-  const boundingBox1 = new THREE.Box3().setFromObject(frontal1);
-  const size1 = boundingBox1.getSize(new THREE.Vector3());
+   frontal1.position.set(center.x - size1.x / 2 - step_size / 2, height / 2, center.z);
 
-  frontal1.position.set(center.x - size1.x / 2 - step_size / 2, height / 2, center.z);
-  //console.log(frontal1.position);
-  //scene.add(frontal1);
+   const frontal2 = new THREE.Mesh(
+      new THREE.BoxGeometry((front_size - step_size) / 2, height, depth),
+      setDefaultMaterial(color)
+   );
 
-  // Plataforma frontal 2
-  const frontal2 = new THREE.Mesh(
-    new THREE.BoxGeometry((front_size - step_size) / 2, height, depth),
-    new THREE.MeshStandardMaterial({ color: color })
-  );
+   const boundingBox2 = new THREE.Box3().setFromObject(frontal2);
+   const size2 = boundingBox2.getSize(new THREE.Vector3());
+   frontal2.position.set(center.x + size2.x / 2 + step_size / 2, height / 2, center.z);
 
-  const boundingBox2 = new THREE.Box3().setFromObject(frontal2);
-  const size2 = boundingBox2.getSize(new THREE.Vector3());
+   const traseira = new THREE.Mesh(
+      new THREE.BoxGeometry(front_size, height, side_size - stair_depth),
+      setDefaultMaterial(color)
+   );
 
-  frontal2.position.set(center.x + size2.x / 2 + step_size / 2, height / 2, center.z);
-  //scene.add(frontal2);
+   traseira.position.set(center.x, height / 2, center.z + (side_size / 2) - (step_depth / 2));
 
-  const traseira = new THREE.Mesh(
-    new THREE.BoxGeometry(front_size, height, side_size - stair_depth),
-    new THREE.MeshStandardMaterial({ color: color })
-  );
+   platform.add(escadaGroup, frontal1, frontal2, traseira);
+   scene.add(platform);
 
-  traseira.position.set(center.x, height / 2, center.z + (side_size / 2) - (step_depth / 2));
-  //scene.add(traseira);
-
-  platform.add(escadaGroup);
-  platform.add(frontal1);
-  platform.add(frontal2);
-  platform.add(traseira);
-
-  return platform;
+   return platform;
 
 }
+
+function createWall(width, height, depth, color) {
+
+   const geometry = new THREE.BoxGeometry(width, height, depth);
+   const material = setDefaultMaterial(color);
+   const wall = new THREE.Mesh(geometry, material);
+
+   return wall;
+}
+
+function addWallsAroundPlane(scene, plane_size, wall_height, wall_thickness, color) {
+
+   const half = plane_size/2;
+
+   const wall_front = createWall(plane_size, wall_height, wall_thickness, color);
+   wall_front.position.set(0, wall_height/2, half + wall_thickness/2);
+
+   const wall_back = createWall(plane_size, wall_height, wall_thickness, color);
+   wall_back.position.set(0, wall_height/2, -half - wall_thickness/2);
+
+   const wall_left = createWall(wall_thickness, wall_height, plane_size, color);
+   wall_left.position.set(-half - wall_thickness/2, wall_height/2, 0);
+
+   const wall_right = createWall(wall_thickness, wall_height, plane_size, color);
+   wall_right.position.set(half + wall_thickness/2, wall_height/2, 0);
+
+   scene.add(wall_front, wall_back, wall_left, wall_right);  
+
+}
+
