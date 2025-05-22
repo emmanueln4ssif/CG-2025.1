0893b48a74a584cc9b1bc75d64cd94588d1db9e3
Emmanuel Nassif <emmanuel.nassif@gmail.com>
2025-05-22 20:46:56 -0300

    fix: change renderer color

--- T1/T1.js
+++ T1/T1.js
@@ -16,6 +16,7 @@ import {
 let scene, renderer, camera, material, light, orbit; // Initial variables
 scene = new THREE.Scene();    // Create main scene
 renderer = initRenderer();    // Init a basic renderer
+renderer.setClearColor("rgb(70, 151, 198)");
 camera = initCamera(new THREE.Vector3(10, 0, -100)); // Init camera in this position
 material = setDefaultMaterial(); // create a basic material
 light = initDefaultBasicLight(scene); // Create a basic light to illuminate the scene
@@ -26,7 +27,7 @@ var keyboard = new KeyboardState();
 // Listen window size changes
 window.addEventListener('resize', function () { onWindowResize(camera, renderer) }, false);
 
-// ** Creating the plane ** 
+//Creating the plane  
 let plane = createGroundPlaneXZ(500, 500)
 plane.material.color.set(0xFFF1C1);
 scene.add(light)
@@ -45,7 +46,7 @@ area4.rotateY(-Math.PI);
 area4.position.set(10, 0, -300);
 
 //Adicionando as paredes (depois precisa aperfeiçoar isso)
-const wall_height = 60;
+const wall_height = 40;
 const wall_thickness = 5;
 const wallColor = 0x8B4513;
 addWallsAroundPlane(scene, 500, wall_height, wall_thickness, wallColor);
@@ -55,7 +56,7 @@ render();
 function render() {
   keyboardUpdate();
   requestAnimationFrame(render);
-  renderer.render(scene, camera) // Render scene
+  renderer.render(scene, camera)
 }
 
 ///////////////////////////////////////////////////////////////////////////////
@@ -80,7 +81,7 @@ function buildPlatform(scene, side_size, front_size, height, position, step_size
     escadaGroup.add(degrau);
   }
 
-  escadaGroup.position.set(position.x, 0, position.z - (side_size/2) + (step_depth/2));
+  escadaGroup.position.set(position.x, 0, position.z - (side_size / 2) + (step_depth / 2));
   //console.log(escadaGroup.position);
 
   const boundingBox = new THREE.Box3().setFromObject(escadaGroup);
@@ -88,29 +89,29 @@ function buildPlatform(scene, side_size, front_size, height, position, step_size
   const center = boundingBox.getCenter(new THREE.Vector3());
 
   const frontal1 = new THREE.Mesh(
-    new THREE.BoxGeometry((front_size - step_size)/2, height, depth),
+    new THREE.BoxGeometry((front_size - step_size) / 2, height, depth),
     setDefaultMaterial(color)
   );
 
   const boundingBox1 = new THREE.Box3().setFromObject(frontal1);
   const size1 = boundingBox1.getSize(new THREE.Vector3());
-  frontal1.position.set(center.x - size1.x/2 - step_size/2, height/2, center.z);
+  frontal1.position.set(center.x - size1.x / 2 - step_size / 2, height / 2, center.z);
 
   const frontal2 = new THREE.Mesh(
-    new THREE.BoxGeometry((front_size - step_size)/2, height, depth),
+    new THREE.BoxGeometry((front_size - step_size) / 2, height, depth),
     setDefaultMaterial(color)
   );
 
   const boundingBox2 = new THREE.Box3().setFromObject(frontal2);
   const size2 = boundingBox2.getSize(new THREE.Vector3());
-  frontal2.position.set(center.x + size2.x/2 + step_size/2, height/2, center.z);
+  frontal2.position.set(center.x + size2.x / 2 + step_size / 2, height / 2, center.z);
 
   const traseira = new THREE.Mesh(
     new THREE.BoxGeometry(front_size, height, side_size - stair_depth),
     setDefaultMaterial(color)
   );
 
-  traseira.position.set(center.x, height/2, center.z + (side_size/2) - (step_depth/2));
+  traseira.position.set(center.x, height / 2, center.z + (side_size / 2) - (step_depth / 2));
 
   platform.add(escadaGroup, frontal1, frontal2, traseira);
   scene.add(platform);
@@ -129,19 +130,19 @@ function createWall(width, height, depth, color) {
 
 function addWallsAroundPlane(scene, plane_size, wall_height, wall_thickness, color) {
 
-  const half = plane_size/2;
+  const half = plane_size / 2;
 
   const wall_front = createWall(plane_size + 10, wall_height, wall_thickness, color);
-  wall_front.position.set(0, wall_height/2, half + wall_thickness/2);
+  wall_front.position.set(0, wall_height / 2, half + wall_thickness / 2);
 
   const wall_back = createWall(plane_size + 10, wall_height, wall_thickness, color);
-  wall_back.position.set(0, wall_height/2, -half - wall_thickness/2);
+  wall_back.position.set(0, wall_height / 2, -half - wall_thickness / 2);
 
   const wall_left = createWall(wall_thickness, wall_height, plane_size, color);
-  wall_left.position.set(-half - wall_thickness/2, wall_height/2, 0);
+  wall_left.position.set(-half - wall_thickness / 2, wall_height / 2, 0);
 
   const wall_right = createWall(wall_thickness, wall_height, plane_size, color);
-  wall_right.position.set(half + wall_thickness/2, wall_height/2, 0);
+  wall_right.position.set(half + wall_thickness / 2, wall_height / 2, 0);
 
   scene.add(wall_front, wall_back, wall_left, wall_right);
 
@@ -149,14 +150,14 @@ function addWallsAroundPlane(scene, plane_size, wall_height, wall_thickness, col
 
 //provisório 
 function keyboardUpdate() {
-   keyboard.update();
-   if (keyboard.pressed("left")) camera.position.x += 1;
-   if (keyboard.pressed("right")) camera.position.x -= 1;
-   if (keyboard.pressed("up")) camera.position.z += 1;
-   if (keyboard.pressed("down")) camera.position.z -= 1;
-   if (keyboard.pressed("pagedown")) camera.position.y -= 1;
-   if (keyboard.pressed("pageup")) camera.position.y += 1;
-  
+  keyboard.update();
+  if (keyboard.pressed("left")) camera.position.x += 1;
+  if (keyboard.pressed("right")) camera.position.x -= 1;
+  if (keyboard.pressed("up")) camera.position.z += 1;
+  if (keyboard.pressed("down")) camera.position.z -= 1;
+  if (keyboard.pressed("pagedown")) camera.position.y -= 1;
+  if (keyboard.pressed("pageup")) camera.position.y += 1;
+
 }
 
 
